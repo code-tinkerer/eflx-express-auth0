4f991d679bebc525682f34f7f8bdf64603603efe
Code Tinkerer <tinkering.with.code@gmail.com>
2018-11-09 14:18:49 -0500

    initial code for getting access token from auth0

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log("Serving the EdFlix application");
+    console.log("Application started");
 });
 
 // code from https://auth0.com/docs/libraries/auth0js/v9
